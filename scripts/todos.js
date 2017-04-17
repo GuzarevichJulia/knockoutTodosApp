73e5d66f0280505d9f4e3cd99297dc424c4045de
yuliya.huzarevich <guzarevichjulie@mail.ru>
2017-04-17 15:24:28 +0300

    Added filters

--- scripts/todos.js
+++ scripts/todos.js
@@ -1,11 +1,21 @@
 var storage;
-var todosFromStorage;
+var allTodosFromStorage;
 var sha1;
 
 function init() {
     storage = storageManager;
-    todosFromStorage = storage.getTodos();
-    sha1 = new Hashes.SHA1
+    allTodosFromStorage = storage.getTodos();
+    sha1 = new Hashes.SHA1;
+}
+
+function updateArrays() {
+    viewModel.activeTodos(viewModel.todos().filter(function (t) {
+        return t.isCompleted == false;
+    }));
+    viewModel.completedTodos(viewModel.todos().filter(function (t) {
+        return t.isCompleted == true;
+    }));
+    viewModel.activeCount(viewModel.activeTodos().length);
 }
 
 function TodoItem(id, content, isDone) {
@@ -14,29 +24,33 @@ function TodoItem(id, content, isDone) {
     this.isCompleted = isDone;
 }
 
-function ViewModel() {
-    var that = this;
-    that.newTodoHandler = function (data, event) {
-        var inputElement = event.currentTarget;
-        if (event.keyCode === 13) {
-            addTodo(inputElement.value);
-            inputElement.value="";
-        }
-        return true;
-    };
+function completeTodo(id) {
+    var todo = ko.utils.arrayFirst(viewModel.todos(), function (t) {
+        return t.id == id;
+    });
 
-    that.destroyHandler = function (data, event) {
-        deleteTodo(event.currentTarget.name);
-        return true;
+    if(todo.isCompleted == true) {
+        todo.isCompleted = false;
     }
-
-    that.todos = ko.observableArray(todosFromStorage)
+    else {
+        todo.isCompleted = true;
+    }
+    updateArrays();
+    storage.save(viewModel.todos);
+    console.log(viewModel.todos());
 }
 
 function deleteTodo(id) {
-    viewModel.todos.remove(function (todo) {
-        return todo.id == id;
+    var todo = ko.utils.arrayFirst(viewModel.todos(), function (t) {
+        return t.id == id;
     });
+    if (todo.isCompleted == false){
+        var previousAllCount = viewModel.activeCount();
+        viewModel.activeCount(previousAllCount - 1);
+    }
+    viewModel.todos.remove(todo);
+    updateArrays();
+    viewModel.displayedTodos(viewModel.todos());
     storage.save(viewModel.todos);
 }
 
@@ -44,10 +58,68 @@ function addTodo (value) {
     var id = sha1.hex(value);
     var newTodo = new TodoItem(id, value, false);
     viewModel.todos.push(newTodo);
+    viewModel.displayedTodos(viewModel.todos());
     storage.save(viewModel.todos);
+    updateArrays();
+}
+
+function clearCompletedTodos() {
+    for(var i = 0; i < viewModel.completedTodos().length; i++){
+        viewModel.todos.remove(viewModel.completedTodos()[i]);
+    }
+    storage.save(viewModel.todos);
+    updateArrays();
+    viewModel.displayedTodos(viewModel.todos());
 }
 
 init();
-var viewModel = new ViewModel();
+
+var viewModel = {
+    that : this,
+    newTodoHandler : function (data, event) {
+        var inputElement = event.currentTarget;
+        if (event.keyCode === 13) {
+            addTodo(inputElement.value);
+            inputElement.value="";
+        }
+        return true;
+    },
+
+    completeHandler : function (data, event) {
+        var element = event.currentTarget;
+        completeTodo(element.name);
+        return true;
+    },
+
+    destroyHandler : function (data, event) {
+        deleteTodo(event.currentTarget.name);
+        return true;
+    },
+
+    showAll : function () {
+        this.displayedTodos(this.todos());
+    },
+
+    showActive : function () {
+        this.displayedTodos(this.activeTodos());
+    },
+
+    showCompleted : function () {
+        this.displayedTodos(this.completedTodos());
+    },
+
+    clearCompleted : function () {
+        clearCompletedTodos();
+        return true;
+    },
+
+    todos : ko.observableArray(allTodosFromStorage),
+    activeTodos : ko.observableArray(),
+    completedTodos : ko.observableArray(),
+    displayedTodos : ko.observableArray(allTodosFromStorage),
+    activeCount : ko.observable(0)
+}
+
+updateArrays();
 ko.applyBindings(viewModel);
 
